6e8228a98383dba4fe90033d3a6f0c391a7df803
Javier Gomez <gomez.javier@opendeusto.es>
2023-03-09 09:33:26 +0100

    Añadido transform file

---
+++ Assets/transformScript.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transformScript : MonoBehaviour
+{
+    int dir = 1;
+    int drops = 0;
+    GameObject[] wagons;
+    float speed = 2.0f;
+    float rotateSpeed = 90.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        wagons = GameObject.FindGameObjectsWithTag("Wagon");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoverTeclado();
+        EscalarTeclado();
+    }
+
+
+    private void EscalarTeclado()
+    {
+        Vector3 esc = new Vector3();
+        if (Input.GetKey(KeyCode.H))
+        {
+            esc.x = 1.0f;
+        }
+        if (Input.GetKey(KeyCode.K))
+        {
+            esc.x = -1.0f;
+        }
+        if (Input.GetKey(KeyCode.U))
+        {
+            esc.y = 1.0f;
+        }
+        if (Input.GetKey(KeyCode.J))
+        {
+            esc.y = -1.0f;
+        }
+        this.transform.localScale += esc*4*Time.deltaTime;
+    }
+
+    private void MoverTeclado()
+    {
+        Vector3 mov = new Vector3();
+        if (Input.GetKey(KeyCode.A))
+        {
+            mov.x = 1.0f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            mov.x = -1.0f;
+        }
+        transform.Translate(mov * speed * Time.deltaTime);
+    }
+}
