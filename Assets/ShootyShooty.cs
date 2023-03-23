48b8223719c6faf55111613901ed0173d4c3c934
Javier Gomez <gomez.javier@opendeusto.es>
2023-03-23 09:39:43 +0100

    Raycast Things

--- Assets/ShootyShooty.cs
+++ Assets/ShootyShooty.cs
@@ -6,17 +6,34 @@ public class ShootyShooty : MonoBehaviour
 {
     public GameObject Bullet;
     public float BulletForce;
+    public bool shooting;
+    public bool shootOnce;
     float InitDelay = 1;
     float delay;
     // Start is called before the first frame update
     void Start()
     {
-        
+        shooting = false;
+        shootOnce = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.S))
+        {
+            shooting = true;
+            
+        }
+        else
+        {
+            shooting = false;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            shootOnce = true;
+        }
+
         if (delay > 0)
         {
             delay -= Time.deltaTime;
@@ -31,5 +48,33 @@ public class ShootyShooty : MonoBehaviour
                 SpawnBullet.GetComponent<Rigidbody>().AddForce(SpawnBullet.transform.forward* BulletForce);
             }
         }
+    }
+
+    void FixedUpdate()
+    {
+        if (shooting==true && shootOnce==true)
+        {
+            shootOnce = false;
+            // Bit shift the index of the layer (8) to get a bit mask
+            int layerMask = 1 << 8;
+
+            // This would cast rays only against colliders in layer 8.
+            // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+            layerMask = ~layerMask;
+
+            RaycastHit hit;
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                Debug.Log("Did Hit " + hit.transform.gameObject.name);
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+                Debug.Log("Did not Hit");
+            }
+        }
+        
     }
 }
